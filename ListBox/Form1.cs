b288eaee227ea67721d4133a7f4b32c6e9ba1fa7
Gihun Ham <progh2@gmail.com>
2019-05-21 00:59:55 +0900

    #2 코드 구현

--- ListBox/Form1.cs
+++ ListBox/Form1.cs
@@ -11,9 +11,35 @@ namespace ListBox
 {
     public partial class Form1 : Form
     {
+        string OrgStr = ""; // 결과 : 저장
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            if(this.txtList.Text != "")
+            {
+                this.lbView.Items.Add(this.txtList.Text);
+                this.txtList.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("아이템을 입력하세요", "알림", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtList.Focus();
+            }
+        }
+
+        private void LbView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lblResult.Text = this.OrgStr + this.lbView.SelectedItem.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lblResult.Text;
         }
     }
 }
